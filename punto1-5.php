d16a8c5d6ccd3ceb08b27fa3d04e490b3776e14d
marina1007 <marinita.toranzo@gmail.com>
2017-09-27 20:27:00 -0300

    ejercicio 1-5 resuelto

---
+++ punto1-5.php
@@ -0,0 +1,23 @@
+<?php
+ 
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$mes = 5; //declaracion de una variable de tipo integer
+$gastos = 2500.5; //declaracion de una variable de tipo double
+$nombre = "Benjamin"; // declaracion de una variable de tipo string,
+$exite = true; // declaracion de una variable de tipo boolean,
+
+//escribo la palabra echo para que imprima por pantalla una cadena de 
+//caracteres
+echo " La variable de tipo entera es: ";
+echo $mes;
+echo " La variable de tipo double es: ";
+echo $gastos;
+echo " La variable de tipo String es: ";
+echo $nombre;
+echo " La variable de tipo boolean es: ";
+echo $exite;
