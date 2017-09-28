e61afcb10e997daaff586456bacda56b8c0bf705
marina1007 <marinita.toranzo@gmail.com>
2017-09-27 21:19:09 -0300

    ejercicio 1-5 resuelto

---
+++ punto1-6.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$dia = 7;
+$mes = 10;
+$anio = 1981;
+
+echo "Marina nacio el dia $dia del mes $mes en el anio $anio";
